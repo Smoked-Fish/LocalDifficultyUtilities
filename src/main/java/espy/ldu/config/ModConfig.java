b933445659c025cfccb0f79154fdfdf98e4dca87
Espy <espy.jacob@gmail.com>
2025-05-24 20:27:38 -0700

    Changed syncInhabitedOnNewChunk default to false

--- src/main/java/espy/ldu/config/ModConfig.java
+++ src/main/java/espy/ldu/config/ModConfig.java
@@ -20,7 +20,7 @@ public class ModConfig implements ConfigData {
 
     public static class Server {
         public boolean syncInhabitedOnF3 = true;
-        public boolean syncInhabitedOnNewChunk = true;
+        public boolean syncInhabitedOnNewChunk = false;
         public int regionMaxChunks = 250001;
         public int regionBatchSize = 100;
     }
